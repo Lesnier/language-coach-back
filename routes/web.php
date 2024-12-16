7683f6e5274abd0bbeaea880ea528badefec7ce8
hectorsolpest <hectorsolpest@gmail.com>
2024-12-15 21:58:58 -0600

    Resolve 404 error in admin routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use TCG\Voyager\Facades\Voyager;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +17,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
 });
