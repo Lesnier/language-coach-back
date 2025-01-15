5a39314e423b72106a898e53414a1a36780411d6
hectorsolpest <hectorsolpest@gmail.com>
2025-01-14 22:17:28 -0600

    Add validation to select only users with role professor in agenda professor

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
@@ -28,11 +29,21 @@ class AppServiceProvider extends ServiceProvider
         Validator::extend('not_weekend', function($attribute, $value, $parameters, $validator) {
             return $this->isWeekend($value) === false;
         });
+        Validator::extend('is_professor', function($attribute, $value, $parameters, $validator) {
+            return $this->isProfessor($value) === true;
+        });
     }
 
     function isWeekend($date) {
         $weekday = date('N', strtotime($date));
         return ($weekday >= 6); // 6 y 7 representan sábados y domingos
+    }
+
+    function isProfessor($user_id)
+    {
+        $user = User::find($user_id);
+        $rol = $user->role->id;
+        return $rol == 3;
     }
 
 }
