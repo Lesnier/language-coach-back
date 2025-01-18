1e44f316b9c1a31e80f231d61e06ae8fc584126e
hectorsolpest <hectorsolpest@gmail.com>
2025-01-17 19:08:49 -0600

    Model creation for bills and products

---
+++ app/Models/Product.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class Product extends Model
+{
+    protected $guarded = [];
+    public function bills()
+    {
+        return $this->belongsToMany(Bill::class, 'bill_product');
+    }
+}
