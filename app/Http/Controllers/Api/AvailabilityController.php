913247e8600f4d9181c02717b0982ee514693a26
Lesnier Gonzalez <lesinnov@gmail.com>
2025-04-18 16:49:02 -0500

    fix: Get Availabilties

--- app/Http/Controllers/Api/AvailabilityController.php
+++ app/Http/Controllers/Api/AvailabilityController.php
@@ -11,7 +11,7 @@ class AvailabilityController extends Controller
     public function getAvailability(Request $request)
     {
         $availabilities = Availability::where('is_available', true)
-            ->select(['id', 'user_id', 'date', 'start_time', 'end_time'])
+            ->select(['id', 'user_id', 'day_of_week', 'start_time', 'end_time'])
             ->get();
 
         // Transform the availabilities to rename user_id to professor_id and drop the user object
